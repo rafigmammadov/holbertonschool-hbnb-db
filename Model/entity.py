18301d680381fce5da1af9adced6eeda4c0ca85e
Rafig <rafigmammadov@yahoo.com>
2024-07-06 22:39:06 +0400

    Entity model has been integrated with SQLAlchemy

--- Model/entity.py
+++ Model/entity.py
@@ -17,9 +17,6 @@ db = SQLAlchemy()
 class Entity(db.Model):
     __tablename__ = 'entities'
     
-    id = db.Column(db.String(36), primary_key=True,
-                    default=lambda: str(uuid.uuid4()))
-    
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     updated_at = db.Column(db.DateTime, nullable=False,
