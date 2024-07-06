111570823dfd5306fd2baffd2bd47295556060a3
Rafig <rafigmammadov@yahoo.com>
2024-07-07 02:51:30 +0400

    Amenity model has been implemented with SQLAlchemy

---
+++ Model/amenity.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module that contains Amenity Model
+"""
+from .entity import Entity, db
+import json
+
+
+class Amenity(Entity):
+    __tablename__ = 'amenities'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return (f"Amenity(name={self.name}, description='{self.description}')")
+
+    def to_dict(self):
+        data = super().to_dict()
+        data.update({
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        })
+        return data
