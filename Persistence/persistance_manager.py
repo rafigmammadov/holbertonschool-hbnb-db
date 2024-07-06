373b01ba032ab5e5c46195042881ff5a49e57c40
Rafig <rafigmammadov@yahoo.com>
2024-07-07 03:01:33 +0400

    Abstract class has been implementated

---
+++ Persistence/persistance_manager.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class IPersistenceManager(ABC):
+    @abstractmethod
+    def save(self, entity):
+        pass
+
+    @abstractmethod
+    def get(self, entity_id, entity_type):
+        pass
+
+    @abstractmethod
+    def update(self, entity):
+        pass
+
+    @abstractmethod
+    def delete(self, entity_id, entity_type):
+        pass
+
+    @abstractmethod
+    def get_by_field(self, field, value, entity_type):
+        pass
