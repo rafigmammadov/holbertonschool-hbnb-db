2676887b37dbf07e59c4bb59f6a47f81b986b44b
Rafig <rafigmammadov@yahoo.com>
2024-07-07 17:41:43 +0400

    SQL script for Country model has been implemented

--- SQL_Scripts/create_country_table.sql
+++ SQL_Scripts/create_country_table.sql
@@ -1,4 +1,6 @@
 CREATE TABLE countries (
     code VARCHAR(36) PRIMARY KEY,
-    name VARCHAR(128) NOT NULL
+    name VARCHAR(128) NOT NULL,
+    country_code VARCHAR(36),
+    FOREIGN KEY (country_code) REFERENCES countries(code)
 );
