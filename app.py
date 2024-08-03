6327cdb2d3fccb945b3d63e3f63fb9f1e64adace
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-08-03 12:47:51 +0400

    holbertonschool-hbnb-client

--- app.py
+++ app.py
@@ -15,18 +15,11 @@ from API.api_amenity import ns_amenities
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable to suppress warning
-
 app.config['JWT_SECRET_KEY'] = 'aminarufatrafig'
 jwt = JWTManager(app)
 
 # Initialize SQLAlchemy instance
 db.init_app(app)
-
-# Create the development database if it doesn't exist
-if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
-    if not os.path.exists('development.db'):
-        with app.app_context():
-            db.create_all()
 
 # Create API instance
 api = Api(app, version='1.0', title='HBnB API', description='HBnB application')
@@ -37,9 +30,40 @@ api.add_namespace(ns_places)
 api.add_namespace(ns_reviews)
 api.add_namespace(ns_amenities)
 
-# Ensure all models are created
+# Create the development database if it doesn't exist
 with app.app_context():
-    db.create_all()
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
+        if not os.path.exists('development.db'):
+            db.create_all()
+
+# Routes for HTML pages
+@app.route('/index')
+def home():
+    return app.send_static_file('index.html')
+
+@app.route('/login')
+def login():
+    return app.send_static_file('login.html')
+
+@app.route('/place')
+def place():
+    return app.send_static_file('place.html')
+
+@app.route('/add_review')
+def add_review():
+    return app.send_static_file('add_review.html')
+
+@app.route('/place_beach')
+def place_beach():
+    return app.send_static_file('place_beach.html')
+
+@app.route('/place_cozy')
+def place_cozy():
+    return app.send_static_file('place_cozy.html')
+
+@app.route('/place_modern')
+def place_modern():
+    return app.send_static_file('place_modern.html')
 
 if __name__ == '__main__':
     with app.app_context():
